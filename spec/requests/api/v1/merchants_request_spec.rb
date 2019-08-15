58c8fbb3c785981ee4b840fe7cbf5f1d2356397d
Oscar Bellechasse <obellech10@gmail.com>
2019-08-14 23:01:52 -0600

    Update tests to pass after adding serializer, added new tests for find by parameter

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,7 +10,7 @@ describe "Merchants API" do
 
     merchants = JSON.parse(response.body)
 
-    expect(merchants.count).to eq(5)
+    expect(merchants["data"].count).to eq(5)
   end
 
   it "can get one merchant by it's id" do
@@ -21,6 +21,48 @@ describe "Merchants API" do
     merchant = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(merchant["id"]).to eq(id)
+    expect(merchant["data"]["attributes"]["id"]).to eq(id)
+  end
+
+  it "can find merchant by id" do
+    created_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?id=#{created_merchant.id}"
+
+    merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["id"]).to eq(created_merchant.id)
+  end
+
+  it "can find merchant by name" do
+    created_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?name=#{created_merchant.name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["name"]).to eq(created_merchant.name)
+  end
+
+  it "can find merchant by created at parameter" do
+    created_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?created_at=#{created_merchant.created_at}"
+
+    merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["created_at"].to_date).to eq(created_merchant.created_at.to_date)
+  end
+
+  it "can find merchant by updated at parameter" do
+    created_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?updated_at=#{created_merchant.updated_at.to_date}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["updated_at"].to_date).to eq(created_merchant.updated_at.to_date)
   end
 end
