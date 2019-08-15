7bf53aa725353c4b8b18e322ba2cb0f55412ad97
Oscar Bellechasse <obellech10@gmail.com>
2019-08-14 23:04:24 -0600

    update routes for search by parameters and BI revenue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+        get '/:id/revenue', to: 'revenue#show'
+      end
+
       resources :merchants, only: [:index, :show]
     end
   end
