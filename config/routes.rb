38852818f6455cb385675d6c2895add5a2515cf2
Oscar Bellechasse <obellech10@gmail.com>
2019-08-15 14:33:18 -0600

    Update for find_all, merchant items, and merchant invoices

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+        get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
         get '/:id/revenue', to: 'revenue#show'
       end
 
