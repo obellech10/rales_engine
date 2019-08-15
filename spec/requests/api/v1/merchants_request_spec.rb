6f5bc25e90a17be3dd1d51122f4c188ed47d7fdc
Oscar Bellechasse <obellech10@gmail.com>
2019-08-15 14:34:21 -0600

    Add tests for single and multiple finds on merchant parameters

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -25,44 +25,104 @@ describe "Merchants API" do
   end
 
   it "can find merchant by id" do
-    created_merchant = create(:merchant)
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
 
-    get "/api/v1/merchants/find?id=#{created_merchant.id}"
+    get "/api/v1/merchants/find?id=#{merchant_1.id}"
 
     merchant = JSON.parse(response.body)
+
     expect(response).to be_successful
-    expect(merchant["data"]["attributes"]["id"]).to eq(created_merchant.id)
+    expect(merchant["data"]["attributes"]["id"]).to eq(merchant_1.id)
   end
 
   it "can find merchant by name" do
-    created_merchant = create(:merchant)
+    merchant_1 = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "flower shop", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_3 = Merchant.create(name: "drinks, drinks, drinks", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
 
-    get "/api/v1/merchants/find?name=#{created_merchant.name}"
+    get "/api/v1/merchants/find?name=#{merchant_1.name}"
 
     merchant = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(merchant["data"]["attributes"]["name"]).to eq(created_merchant.name)
+    expect(merchant["data"]["attributes"]["name"]).to eq(merchant_1.name)
   end
 
   it "can find merchant by created at parameter" do
-    created_merchant = create(:merchant)
+    created_merchant = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
 
     get "/api/v1/merchants/find?created_at=#{created_merchant.created_at}"
 
     merchant = JSON.parse(response.body)
+
     expect(response).to be_successful
     expect(merchant["data"]["attributes"]["created_at"].to_date).to eq(created_merchant.created_at.to_date)
   end
 
   it "can find merchant by updated at parameter" do
-    created_merchant = create(:merchant)
+    created_merchant = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
 
-    get "/api/v1/merchants/find?updated_at=#{created_merchant.updated_at.to_date}"
+    get "/api/v1/merchants/find?updated_at=#{created_merchant.updated_at}"
 
     merchant = JSON.parse(response.body)
 
     expect(response).to be_successful
     expect(merchant["data"]["attributes"]["updated_at"].to_date).to eq(created_merchant.updated_at.to_date)
+  end
+
+  it "can find all merchants by id" do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+
+    get "/api/v1/merchants/find_all?id=#{merchant_2.id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"].first["attributes"]["id"]).to eq(merchant_2.id)
+  end
+
+  it "can find all merchants by name" do
+    merchant_1 = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+
+    get "/api/v1/merchants/find_all?name=#{"banana stand"}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"].count).to eq(3)
+  end
+
+  it "can find all merchants by created at date" do
+    merchant_1 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+
+    get "/api/v1/merchants/find_all?created_at=#{"2019-08-14 15:38:30 UTC"}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"].count).to eq(4)
+  end
+
+  it "can find all merchants by updated at date" do
+    merchant_1 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-14 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    merchant_2 = Merchant.create(name: "banana stand", created_at: "2019-08-14 15:38:30 UTC", updated_at: "2019-08-14 15:38:30 UTC")
+
+    get "/api/v1/merchants/find_all?updated_at=#{"2019-08-15 15:38:30 UTC"}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"].count).to eq(2)
   end
 end
