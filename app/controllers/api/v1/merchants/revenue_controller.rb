4196d37dbe4d272506cac40a3fa6766e4daed9a2
Oscar Bellechasse <obellech10@gmail.com>
2019-08-17 17:53:05 -0600

    Update show method, code works but is not returning the correct number format

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    # binding.pry
-    # render json: MerchantSerializer.new(Merchant.revenue(params[:date]))
+    binding.pry
+    revenue = Merchant.find(params[:id]).total_revenue
+    render json: MerchantSerializer.new(revenue)
     # render json: RevenueSerializer.new(Merchant.revenue(params[:date]))
   end
 end
