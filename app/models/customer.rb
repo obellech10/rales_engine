d83dca588833917431ffa748f14a087d8067bf02
Oscar Bellechasse <obellech10@gmail.com>
2019-08-17 17:13:49 -0600

    Add favorite merchant method to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
 
   validates_presence_of :first_name,
                         :last_name
+
+  def favorite_merchant
+    Merchant.joins(invoices: :transactions)
+      .select("merchants.*, COUNT(invoices.merchant_id) AS invoice_count")
+      .where(invoices: {customer_id: id})
+      .merge(Transaction.successful)
+      .group("merchants.id")
+      .order("invoice_count DESC").first
+  end
 end
