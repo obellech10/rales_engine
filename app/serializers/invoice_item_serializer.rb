b53f6551682538d06e2b678021ef596b15a972fd
Oscar Bellechasse <obellech10@gmail.com>
2019-08-18 12:55:28 -0600

    Update to remove timestamps from attributes, added code to transform unit_price to have decimal places

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,7 +1,11 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
 
   belongs_to :item
   belongs_to :invoice
+
+  attribute :unit_price do |object|
+    "#{'%.2f' % (object.unit_price/100.0)}"
+  end
 end
