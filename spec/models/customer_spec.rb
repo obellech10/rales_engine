0554102918e3dd51a8baac74078527693c9565bc
Oscar Bellechasse <obellech10@gmail.com>
2019-08-17 20:11:58 -0600

    Add test for transaction relationship

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   describe 'Relationships' do
     it {should have_many :invoices}
+    it {should have_many(:transactions).through(:invoices)}
   end
 
   describe 'Validations' do
