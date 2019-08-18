8767546442a575301f51c970c70f582557016638
Oscar Bellechasse <obellech10@gmail.com>
2019-08-18 12:53:03 -0600

    Create test for record endpoints

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -53,59 +53,120 @@ describe 'Invoice Items Record Endpoints' do
     expect(invoice_item["data"]["attributes"]["id"]).to eq(@invoice_items_1.id)
   end
 
-  it 'Can find a invoice_item by id' do
-    get "/api/v1/invoice_items/find?id=#{@invoice_items_3.id}"
+  describe 'Single Finder Endpoints' do
+    it 'Can find a invoice_item by id' do
+      get "/api/v1/invoice_items/find?id=#{@invoice_items_3.id}"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    invoice_item = JSON.parse(response.body)
+      invoice_item = JSON.parse(response.body)
 
-    expect(invoice_item["data"]["attributes"]["id"]).to eq(@invoice_items_3.id)
+      expect(invoice_item["data"]["attributes"]["id"]).to eq(@invoice_items_3.id)
+    end
+
+    it "Can find an invoice_item by it's invoice_id" do
+      get "/api/v1/invoice_items/find?invoice_id=#{@invoice_items_4.invoice_id}"
+
+      expect(response).to be_successful
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(invoice_item["data"]["attributes"]["invoice_id"]).to eq(@invoice_items_4.invoice_id)
+    end
+
+    it "Can find an invoice item by it's item_id" do
+      get "/api/v1/invoice_items/find?item_id=#{@invoice_items_4.item_id}"
+
+      expect(response).to be_successful
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(invoice_item["data"]["attributes"]["item_id"]).to eq(@invoice_items_4.item_id)
+    end
+
+    it "Can find an invoice item by it's created_at date" do
+      invoice_items_6 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2019-06-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+      invoice_items_7 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2019-07-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+
+      get "/api/v1/invoice_items/find?created_at=#{invoice_items_6.created_at}"
+
+      expect(response).to be_successful
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(invoice_item["data"]["attributes"]["id"]).to eq(invoice_items_6.id)
+    end
+
+    it "Can find an invoice item by it's updated_at date" do
+      invoice_items_6 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2019-06-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+      invoice_items_7 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2019-07-15 15:38:30 UTC", updated_at: "2019-04-15 15:38:30 UTC")
+
+      get "/api/v1/invoice_items/find?updated_at=#{invoice_items_7.updated_at}"
+
+      expect(response).to be_successful
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(invoice_item["data"]["attributes"]["id"]).to eq(invoice_items_7.id)
+    end
   end
 
-  it "Can find an invoice_item by it's invoice_id" do
-    get "/api/v1/invoice_items/find?invoice_id=#{@invoice_items_4.invoice_id}"
+  describe "Multi-finder Endpoints" do
+    it 'Can find all invoice_item by id' do
+      get "/api/v1/invoice_items/find_all?id=#{@invoice_items_3.id}"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    invoice_item = JSON.parse(response.body)
+      invoice_items = JSON.parse(response.body)
 
-    expect(invoice_item["data"]["attributes"]["invoice_id"]).to eq(@invoice_items_4.invoice_id)
-  end
+      expect(invoice_items["data"].first["attributes"]["id"]).to eq(@invoice_items_3.id)
+    end
 
-  it "Can find an invoice item by it's item_id" do
-    get "/api/v1/invoice_items/find?item_id=#{@invoice_items_4.item_id}"
+    it "Can find all invoice_item by it's invoice_id" do
+      get "/api/v1/invoice_items/find_all?invoice_id=#{@invoice_items_4.invoice_id}"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    invoice_item = JSON.parse(response.body)
+      invoice_items = JSON.parse(response.body)
 
-    expect(invoice_item["data"]["attributes"]["item_id"]).to eq(@invoice_items_4.item_id)
-  end
+      expect(invoice_items["data"].first["attributes"]["invoice_id"]).to eq(@invoice_items_4.invoice_id)
+    end
 
-  it "Can find an invoice item by it's created_at date" do
-    invoice_items_4 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2019-06-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
-    invoice_items_5 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2019-07-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
+    it "Can find all invoice items by it's item_id" do
+      get "/api/v1/invoice_items/find_all?item_id=#{@invoice_items_2.item_id}"
 
-    get "/api/v1/invoice_items/find?created_at=#{invoice_items_4.created_at}"
+      expect(response).to be_successful
 
-    expect(response).to be_successful
+      invoice_items = JSON.parse(response.body)
 
-    invoice_item = JSON.parse(response.body)
+      expect(invoice_items["data"].first["attributes"]["item_id"]).to eq(@invoice_items_2.item_id)
+    end
 
-    expect(invoice_item["data"]["attributes"]["id"]).to eq(invoice_items_4.id)
-  end
+    it "Can find all invoice items by it's created_at date" do
+      invoice_items_6 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2018-06-15 15:38:30 UTC", updated_at: "2018-08-15 15:38:30 UTC")
+      invoice_items_7 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2018-06-15 15:38:30 UTC", updated_at: "2018-08-15 15:38:30 UTC")
 
-  it "Can find an invoice item by it's updated_at date" do
-    invoice_items_4 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2019-06-15 15:38:30 UTC", updated_at: "2019-08-15 15:38:30 UTC")
-    invoice_items_5 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2019-07-15 15:38:30 UTC", updated_at: "2019-04-15 15:38:30 UTC")
+      get "/api/v1/invoice_items/find_all?created_at=#{invoice_items_6.created_at}"
 
-    get "/api/v1/invoice_items/find?updated_at=#{invoice_items_5.updated_at}"
+      expect(response).to be_successful
 
-    expect(response).to be_successful
+      invoice_items = JSON.parse(response.body)
 
-    invoice_item = JSON.parse(response.body)
+      expect(invoice_items["data"].count).to eq(2)
+    end
 
-    expect(invoice_item["data"]["attributes"]["id"]).to eq(invoice_items_5.id)
+    it "Can find all invoice items by it's updated_at date" do
+      invoice_items_6 = @invoice_4.invoice_items.create!(item_id: @item_1.id, quantity: 2, unit_price: @item_1.unit_price, created_at: "2018-06-15 15:38:30 UTC", updated_at: "2018-08-15 15:38:30 UTC")
+      invoice_items_7 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2018-06-15 15:38:30 UTC", updated_at: "2018-08-15 15:38:30 UTC")
+      invoice_items_8 = @invoice_5.invoice_items.create!(item_id: @item_4.id, quantity: 1, unit_price: @item_4.unit_price, created_at: "2018-06-15 15:38:30 UTC", updated_at: "2018-08-15 15:38:30 UTC")
+
+      get "/api/v1/invoice_items/find_all?updated_at=#{invoice_items_8.updated_at}"
+
+      expect(response).to be_successful
+
+      invoice_items = JSON.parse(response.body)
+
+      expect(invoice_items["data"].count).to eq(3)
+    end
   end
 end
