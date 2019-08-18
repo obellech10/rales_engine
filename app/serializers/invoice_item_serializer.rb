76108d89031a11075d41d9ace0e46ffe1dee6ee4
Oscar Bellechasse <obellech10@gmail.com>
2019-08-17 21:11:11 -0600

    Create invoice item serializer

---
+++ app/serializers/invoice_item_serializer.rb
@@ -0,0 +1,7 @@
+class InvoiceItemSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  belongs_to :item
+  belongs_to :invoice
+end
