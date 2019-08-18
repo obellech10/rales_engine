6a4b276e8757a139adaae2c682ce9bb9abfb32f9
Oscar Bellechasse <obellech10@gmail.com>
2019-08-18 12:57:13 -0600

    Update to include method to adj unit price to not have decimals in the params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def adj_unit_price(parameter)
+    if parameter[:unit_price]
+      parameter[:unit_price] = parameter[:unit_price].gsub('.','')
+    end
+      parameter
+  end
 end
